993831fc2f9f4b6c5fc694602da6467722f5ace5
Tiffany Li <tiffanyjl2000@gmail.com>
2021-12-04 02:12:24 -0800

    GraphTraversal / FindSubset

---
+++ FindSubset.java
@@ -0,0 +1,36 @@
+public class FindSubset {
+    public static void printAllSubsets(char[] input) {
+        findSubset(input, 0, "");
+    }
+
+    private static void findSubset(char[] input, int index, String prefix) {
+        //base case
+        if (index == input.length) {
+            System.out.println(prefix);
+            return;
+        }
+
+//        prefix.append(input[index]);
+//        findSubset(input, index + 1, prefix);
+//        prefix.deleteCharAt(input.length - 1);
+//
+//        prefix.append("");
+//        findSubset(input, index + 1, prefix);
+//        prefix.deleteCharAt(input.length - 1);
+
+        char[] children = new char[]{input[index], Character.MIN_VALUE};
+
+        for (int i = 0; i < children.length; i++) {
+            prefix.append(children[i]);
+            findSubset(input, index + 1, prefix);
+            prefix.deleteCharAt(prefix.length - 1);
+        }
+
+
+//        for (int i = 0; i < input.length; i++) {
+//            prefix.append(input[i]);
+//            findSubset(input, index + 1, prefix);
+//            prefix.deleteCharAt(input.length - 1);
+//        }
+    }
+}
