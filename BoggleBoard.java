dca96c219d5f3e8bfc96d2455227becb3f5c0ac3
Tiffany Li <tiffanyjl2000@gmail.com>
2021-12-23 13:33:00 -0800

    BoggleBoard continues..

--- BoggleBoard.java
+++ BoggleBoard.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BoggleBoard {
     DictionaryNode dictionaryRoot;
@@ -7,11 +10,62 @@ public class BoggleBoard {
     public BoggleBoard(char[][] boggleBoard, String[] dictionary) {
         generateDictionaryTree(dictionary);
         dictionaryRoot.print();
+
     }
 
     //return all words in Boggle Board
-   // public List<String> getAllWords( ) {
-    // }
+    public List<String> getAllWords(char[][] boggleBoard) {
+        List<String> result = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        boolean isValid;
+        //null check if DicTreeNode is null
+        if (dictionaryRoot == null) {
+            return result;
+        }
+        for (int i = 0; i < boggleBoard.length; i++) {
+            for (int j = 0; j < boggleBoard[0].length; j++) {
+                char startingPoint = boggleBoard[i][j];
+                if (dictionaryRoot.children.containsKey(startingPoint)){
+                    sb.append(startingPoint);
+                    helper(i, j, sb, result, dictionaryRoot.children.get(startingPoint), boggleBoard);
+                }
+            }
+        }
+     }
+
+    private void helper(int i, int j, StringBuilder curWord, List<String> result, DictionaryNode curNode, char[][] boggleBoard) {
+
+        //base case
+        if (curNode.isEndOfWord) {
+            result.add(curWord.toString());
+            if (curNode.children.isEmpty()) {
+                return;
+            }
+        }
+
+        for (int[] coordinate : validNex]) [
+                tCells(i, j, curNode)) {
+            char c = boggleBoard[coordinate[0]][coordinate[1]];
+            curWord.append(c);
+            DictionaryNode childNode = curNode.children.get(c);
+            helper(coordinate[0], coordinate[1], curWord, result, childNode, boggleBoard);
+            curWord.deleteCharAt(curWord.length() - 1);
+        }
+    }
+
+    /**
+     *
+     * @param i
+     * @param j
+     * @param curNode
+     * @return A set of integer array, first element corresponds to the row index of a valid next cell,
+     * second element corresponds to the column index of a valid next cell.
+     */
+    private Set<int[]> validNextCells(int i, int j, DictionaryNode curNode) {
+        Set<int[]> result = new HashSet<>();
+        // to be added...
+        return result;
+    }
 
     private void generateDictionaryTree(String[] Dictionary) {
 
